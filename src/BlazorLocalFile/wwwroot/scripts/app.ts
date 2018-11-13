43f035dc0329cb0e49a6f51e19a9094a81acfe32
Jeremy Burman <jburman@gmail.com>
2018-11-13 14:34:25 -0500

    Example for loading a browser local file into blazor via fetch. Hacks around .NET HTTP stack restrictions via custom URL parameter and overriden fetch method in the browser.

---
+++ src/BlazorLocalFile/wwwroot/scripts/app.ts
@@ -0,0 +1,26 @@
+﻿interface IApp {
+    page: any
+    createObjectUrl(files: FileList)
+    showFileSelector(elementId: string, page: any)
+}
+interface Window {
+    app: IApp
+}
+(new function () {
+    let app: IApp = {
+        page: null,
+        createObjectUrl: function (files: FileList) {
+            if (files.length > 0) {
+                let file: File = files[0];
+                let url = window.URL.createObjectURL(file);
+                app.page.invokeMethodAsync('SetBlobUrl', url);
+            }
+        },
+        showFileSelector: function (elementId: string, page: any) {
+            app.page = page;
+            let fileSelector: HTMLElement = document.getElementById(elementId);
+            fileSelector.click();
+        }
+    }
+    window.app = app;
+})();
